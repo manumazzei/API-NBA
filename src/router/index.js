4c7565b31c3a500c6a84e0fcf3d5abe27a402d2d
Douglas Amaral <douglaxz@hotmail.com>
2023-06-05 15:08:10 -0300

    separação das rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Player from "../views/players.vue";
+import Teams from "../views/teams.vue";
+import Home from "../views/home.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/teams",
+      name: "teams",
+      component: Teams,
+    },
+    {
+      path: "/players",
+      name: "plaers",
+      component: Player,
+    },
+  ],
+});
 
-export default router
+export default router;
