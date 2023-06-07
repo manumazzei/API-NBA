46221bf686cdc03047ab2202a6e7cb3136d29a1f
Douglas Amaral <douglaxz@hotmail.com>
2023-06-07 16:11:00 -0300

    finalizando details

--- src/views/playerDetail.vue
+++ src/views/playerDetail.vue
@@ -1,4 +1,5 @@
 <script>
+import nophoto from "../assets/players/nophoto.png";
 export default {
   data() {
     return {
@@ -6,6 +7,7 @@ export default {
       loading: false,
       query: "",
       ulrGoolge: "",
+      id: this.$route.params.id,
     };
   },
   methods: {
@@ -16,9 +18,6 @@ export default {
 
       const { first_name, last_name, position, team } = data;
 
-      const query = `${first_name} ${last_name} NBA player`;
-      const googleUrl = `https://www.google.com/search?q=${query}&tbm=isch`;
-
       (this.person = {
         Name: first_name,
         "Last Name": last_name,
@@ -27,6 +26,27 @@ export default {
         City: team.city,
       }),
         (this.loading = false);
+    },
+    replaceByDefault(e) {
+      e.target.src = nophoto;
+    },
+    getImagePath(id) {
+      return `../src/assets/players/${id}.png`;
+    },
+    fileExists(filename) {
+      console.log(filename);
+      // Cria um novo objeto de requisição
+      var http = new XMLHttpRequest();
+
+      // Define o método e a URL da requisição
+      http.open("HEAD", filename, false);
+
+      // Envia a requisição
+      http.send();
+
+      // Verifica o status da resposta
+      console.log(http.status);
+      return http.status !== 404;
     },
   },
   mounted() {
@@ -37,21 +57,46 @@ export default {
 </script>
 <template>
   <main>
+    <div class="divGeral1">
+      <div class="divNav">
+        <div class="divNavTitulo">
+          <p class="pTituloItem">{{ person.Name }}</p>
+        </div>
+        <div class="divNavBottoes">
+          <RouterLink :to="`/players`"
+            ><span class="material-symbols-sharp"> undo </span></RouterLink
+          >
+        </div>
+      </div>
+      <div class="divProfile">
+        <div class="divCardFotoProfile">
+          <img
+            v-if="fileExists(getImagePath(id))"
+            :src="getImagePath(id)"
+            alt=""
+          />
+          <img v-else src="../assets/players/nophoto.png" alt="" width="200" />
+        </div>
+        <div class="divDadosPlayer">
+          <table v-show="!loading" class="tableProfile">
+            <tr v-for="(value, label) in person" :key="label">
+              <td>{{ label }}</td>
+              <td>{{ value }}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
     <h3 v-show="loading">
       <iframe
         src="https://embed.lottiefiles.com/animation/21271"
         style="pointer-events: none"
       ></iframe>
     </h3>
-    <table v-show="!loading">
-      <tr v-for="(value, label) in person" :key="label">
-        <td>{{ label }}</td>
-        <td>{{ value }}</td>
-      </tr>
-    </table>
-    <div id="button-Container">
-      <RouterLink :to="`/players`"><button>back</button></RouterLink>
-    </div>
   </main>
 </template>
-<style></style>
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
