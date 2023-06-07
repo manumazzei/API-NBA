26d739153a14bcdfc90a8b4fbf6afef3e20e0054
Douglas Amaral <douglaxz@hotmail.com>
2023-06-07 10:36:53 -0300

    finalizando pagina principal

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
 <template>
-  <div class="about">
-    <h1>Pagina sobre jogadores</h1>
+  <div class="divGeral">
+    <div class="divGeralItem">
+      <div class="divTimes">
+        <RouterLink to="/Teams"
+          ><button class="btnAcesso">TEAMS</button></RouterLink
+        >
+      </div>
+    </div>
+    <div class="divGeralItem">
+      <div class="divJogadores">
+        <RouterLink to="/Players"
+          ><button class="btnAcesso">PLAYERS</button></RouterLink
+        >
+      </div>
+    </div>
+    <div class="divGeralItem">
+      <div class="divJogos">
+        <RouterLink to="/Players"
+          ><button class="btnAcesso">GAMES</button></RouterLink
+        >
+      </div>
+    </div>
   </div>
 </template>
 
