6114fd7420e37b920533a06fe7cbac3f889e616f
Douglas Amaral <douglaxz@hotmail.com>
2023-06-12 09:15:09 -0300

    correcao foto

--- src/views/players.vue
+++ src/views/players.vue
@@ -58,7 +58,7 @@ export default {
       e.target.src = nophoto;
     },
     getImagePath(id) {
-      return `../src/assets/players/${id}.png`;
+      return `../src/assets/players/p${id}.png`;
     },
     fileExists(filename) {
       console.log(filename);
