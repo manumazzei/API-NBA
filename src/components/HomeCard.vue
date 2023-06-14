4a53169f7232f87b1dbe6c3a322fc7f02debcb28
Emmanuelle <emmanuellematos9@gmail.com>
2023-06-14 09:53:44 -0300

    cria componentes.

---
+++ src/components/HomeCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props: {
+        route: String,
+        title: String,
+        customClass: String,
+    },
+};
+</script>
+
+<template>
+    <div class="divGeralItem">
+        <div :class="customClass">
+            <RouterLink :to="route">
+                <button class="btnAcesso">{{ title }}</button>
+            </RouterLink>
+        </div>
+    </div>
+</template>
---
+++ src/layout/NavLink.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    props: {
+        route: String,
+        title: String,
+        icon: String,
+    },
+};
+</script>
+
+<template>
+    <RouterLink :to="route">
+        <button class="btnAcessoCabecalho">
+            <span class="material-symbols-sharp">{{ icon }}</span>
+        </button>
+    </RouterLink>
+</template>
