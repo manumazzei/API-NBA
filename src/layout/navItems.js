907d93dec4ceccc918bb9064c09b36ed73e66848
Emmanuelle <emmanuellematos9@gmail.com>
2023-06-14 10:07:42 -0300

    adiciona lista de items de nav.

---
+++ src/layout/navItems.js
@@ -0,0 +1,6 @@
+export default [
+    {route: "/", title: "home", icon: "home"},
+    {route: "/Teams", title: "teams", icon: "groups"},
+    {route: "/Players", title: "players", icon: "sports_handball"},
+    {route: "/Games", title: "last machups", icon: "sports_basketball"},
+]
