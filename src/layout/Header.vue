4248804b53d8a6157a0043e031e450f0ef5ffe76
Emmanuelle <emmanuellematos9@gmail.com>
2023-06-14 10:04:11 -0300

    cria componente Header.

---
+++ src/layout/Header.vue
@@ -0,0 +1,33 @@
+<script>
+import NavLink from "@/layout/NavLink.vue";
+import navItems from "./navItems";
+export default {
+    components: {
+        NavLink,
+    },
+    data() {
+        return {
+            navItems,
+        };
+    },
+};
+</script>
+
+<template>
+    <div class="divCabecalho">
+        <div class="divCabecalhoLogoLocal">
+            <div class="divCabecalhoLogo">
+                <img src="@/assets/nbaLogo.png" height="60" />
+            </div>
+        </div>
+        <div class="divCabecalhoLink">
+            <NavLink
+            v-for="item in navItems"
+            :key="item.route"
+            :route="item.route"
+            :title="item.title"
+            :icon="item.icon"
+            />
+        </div>
+    </div>
+</template>
